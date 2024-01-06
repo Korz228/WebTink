e7a618d0666a47a351f586c864a7a5a080dd515f
Maxim <maxim@gmail.com>
2024-01-06 14:23:56 +0400

    I complete task 3

---
+++ Task3/js-oop-tasks/src/es6.js
@@ -0,0 +1,64 @@
+"use strict";
+// в данных задачах нужно использовать возможности es6
+// ко всем заданиям можно дописать свои тесты в файле es6.spec.js
+// Можно менять параметры функций (например сделать им значения по умолчанию)
+
+// Напишите функцию, которая принимает ФИО пользователя и возвращает
+// строку формата Имя Фамилия
+function fioToName(fio) {
+    const [lastName, firstName] = fio.split(" ");
+    return `${firstName} ${lastName}`;
+}
+
+// преобразуйте массив чисел так, чтобы в нем остались только
+// уникальные элементы
+// присмотритесь к коллекции "Set"
+function filterUnique(array) {
+    const mySet = new Set(array);
+    return Array.from(mySet);
+}
+
+// Задача: разница зарплат
+// в функцию приходит массив из n зарплат сотрудников фирмы
+// ваша задача определить, во сколько раз зарплата самого высокооплачиваемого
+// сотрудника превышает зарплату самого низкооплачиваемого
+// присмотритесь к методу .reduce
+function calculateSalaryDifference(array = []) {
+    if (array.length === 0 )return false;
+
+    const max = array.reduce((max, current) => max > current ? max : current, array[0]);
+    const min = array.reduce((min, current) => min < current ? min : current, array[0]);
+    return max/min;
+}
+
+// Реализуйте класс "словарь слов" (как толковый словарь)
+// класс должен быть безопасным и работать только со словами
+// присмотритесь к коллекции "Map"
+// Словарь - (string, string), и все это не null и не undefined
+// * покройте класс тестами
+class Dictionary {
+    constructor()
+    {
+        this._data = new Map();
+    }
+
+    set(key, data)
+    {
+        if((typeof key === "string") && (typeof data === "string"))
+        {
+            this._data.set(key, data);
+        }
+    }
+
+    get(key)
+    {
+        return this._data.get(key);
+    }
+}
+
+module.exports = {
+    fioToName,
+    filterUnique,
+    Dictionary,
+    calculateSalaryDifference
+};
---
+++ Task3/js-oop-tasks/src/es6.spec.js
@@ -0,0 +1,69 @@
+const assert = require('assert');
+const core = require('./es6');
+
+describe('es6', () => {
+    describe('#fioToName', () => {
+        it('ФИО в Имя Фамилия корректно', () => {
+            assert.strictEqual(core.fioToName('Иванов Иван Иванович'), 'Иван Иванов');
+        });
+
+        it('ФИ в Имя Фамилия', () => {
+            assert.strictEqual(core.fioToName('Петров Петр'), 'Петр Петров');
+        });
+    });
+
+    describe('#filterUnique', () => {
+        it('массив с уникальными равен сам себе', () => {
+            assert.deepStrictEqual(core.filterUnique([1, 2, 3]), [1, 2, 3]);
+        });
+
+        it('массив с неуникальными отфильтрован', () => {
+            assert.deepStrictEqual(core.filterUnique([1, 1, 1, 1]), [1]);
+        });
+
+        it('пустой массив', () => {
+            assert.deepStrictEqual(core.filterUnique([]), []);
+        });
+    });
+
+    describe('#calculateSalaryDifference', () => {
+        it('считает разницу корректно', () => {
+            assert.strictEqual(core.calculateSalaryDifference([1, 2, 3]), 3);
+        });
+
+        it('на пустой массив возвращается falsy значение', () => {
+            assert.strictEqual(!!core.calculateSalaryDifference([]), false);
+        });
+    });
+
+    describe('#Dictionary', () => {
+        it('экземпляр класса создается', () => {
+            const dic = new core.Dictionary();
+
+            // TODO
+            assert.strictEqual(!!dic, true);
+        });
+        it('на несуществвующий ключ возвращается undefined', () => {
+            const dic = new core.Dictionary();
+
+            assert.strictEqual(dic.get(1), undefined);
+            assert.strictEqual(dic.get("b"), undefined);
+        });
+
+        it('на существвующий ключ возвращается его значение', () => {
+            const dic = new core.Dictionary();
+            dic.set("aba", "aba");
+            dic.set("bab", "bab");
+
+            assert.strictEqual(dic.get("aba"), "aba");
+            assert.strictEqual(dic.get("bab"), "bab");
+        });
+
+        it('не строку добавить не получится', () => {
+            const dic = new core.Dictionary();
+            dic.set(9, 3);
+
+            assert.strictEqual(dic.get(9), undefined);
+        });
+    });
+});

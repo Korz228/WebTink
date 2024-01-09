dac648a74dc1712bcbff3b30362051aea2040ddc
Maxim <maxim@gmail.com>
2024-01-10 03:18:36 +0400

    last task

---
+++ Task6/src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+    return (
+      <header className="header">
+        <h1 className="header__title">Фильмы</h1>
+        <div className="header__author">6407 Максим Корзюков</div>
+      </header>
+    );
+  };
+  
+  export default Header;
